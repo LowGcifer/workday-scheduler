7f2fb11fbfe361fe63706ad7070b12bb7a4eaf1f
LowGcifer <LowGcifer@gmail.com>
2021-08-02 09:41:12 -0400

    creating repo

---
+++ script.js
@@ -0,0 +1,12 @@
+// var timeBlocks = $(".container");
+// var currentDay = $("#currentDay");
+// var button = $("<button>");
+// var newInput = $("<i>");
+// var times = $("<p>");
+// var schedule = $("<textarea>");
+// var newDiv = $("<div>");
+
+// for (let i = 12; i <= 24; i++) {
+//   timeBlocks.append(newDiv);
+//   newDiv.addClass("time-block-row");
+// }
